0fe1061a412320d6207f686020070c3f683a15cc
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-05-20 17:32:22 +0530

    Nikhil

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,17 @@
+import React, { useState } from 'react';
+import '../styles/Child.css';
+const Selection = ({ applyColor }) => {
+  const [style, updateSelectionStyle] = useState({ background: "" });
+   
+  return (
+    <div 
+      className="fix-box" 
+      style={style} 
+      onClick={() => applyColor(updateSelectionStyle)}
+    >
+      Selection
+    </div>
+  );
+};
 
+export default Selection;
